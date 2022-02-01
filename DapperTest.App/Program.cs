5dddce285767878bbdb1cd89e12138dc01086908
Pavel Popov <legeon48@mail.ru>
2022-02-01 19:56:06 +0300

    Add project files.

---
+++ DapperTest.App/Program.cs
@@ -0,0 +1,24 @@
+﻿using DepperTest.DB_Lib;
+using System;
+
+namespace DapperTest.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = new DbPerson();
+
+            foreach(var person in db.GetAllPersons())
+            {
+               Console.WriteLine($"#{person.Id} {person.FirstName} {person.LastName}  {person.Age}");
+            }
+
+            Console.WriteLine("\n\n");
+            foreach (var person in db.GetTable("users"))
+            {
+                Console.WriteLine($"#{person.Id}:  {person.FirstName} {person.LastName},  {person.Age}");
+            }
+        }
+    }
+}
---
+++ DepperTest.DB_Lib/DbPerson.cs
@@ -0,0 +1,40 @@
+﻿using Dapper;
+using DepperTest.Model;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace DepperTest.DB_Lib
+{
+    public class DbPerson
+    {
+        private readonly string _str;
+        public DbPerson() 
+        {
+            _str = GetConnectionString("connection_to_db.txt");
+        }
+
+        private static string GetConnectionString(string path)
+        {
+            return File.ReadAllText(path);
+        }
+
+        public List<Person> GetAllPersons()
+        {
+            using var db = new SqlConnection(_str);
+            var sql = "SELECT * FROM users";
+            var persons = db.Query<Person>(sql);
+            return persons.ToList();
+        }
+
+        public List<dynamic> GetTable(string table)
+        {
+            using var db = new SqlConnection(_str);
+            var sql = $"SELECT * FROM {table}";
+            var persons = db.Query(sql);
+            return persons.ToList();
+        }
+    }
+}
